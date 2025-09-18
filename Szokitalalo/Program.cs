c8fceade30b18cc532084483f8f515a625a4675d
OreskoZ <oreskoz@kkszki.hu>
2025-09-18 11:20:50 +0200

    Add project files.

---
+++ Szokitalalo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szokitalalo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            string szo = "szó";
+            Console.WriteLine(szo[1]);
+            string[] szavak = { "első", "alma", "vizibicikli", "kukac"};
+            Random rnd = new Random();
+            Console.WriteLine(szavak[rnd.Next(szavak.Length)]);
+
+        }
+
+    }
+}
